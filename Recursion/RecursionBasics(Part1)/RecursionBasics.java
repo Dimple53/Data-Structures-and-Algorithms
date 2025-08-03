aebf569bb77922c68fccfa5b00e405110e2d0419
Dimple53 <dimplegautam53@gmail.com>
2025-08-03 22:26:29 +0200

    print sum of first n natural numbers

--- Recursion/RecursionBasics(Part1)/RecursionBasics.java
+++ Recursion/RecursionBasics(Part1)/RecursionBasics.java
@@ -21,19 +21,31 @@ public class RecursionBasics {
 
 
   //print factorial of number n -> Tc=O(n), SC = O(n)
-  public static int fact(int n){
+  public static int fact(int n) {
     if (n == 0) {
       return 1;
     }
     int fnm1 = fact(n - 1);
     int fn = fnm1 * n;
-    return fn; 
+    return fn;
   }
+  
+  //print sum of first n natural numbers -> Tc=O(n), SC = O(n)
+  public static int calSum(int n) {
+    if (n == 1) {
+      return 1;
+    }
+    int Snm1 = calSum(n - 1);
+    int Sn = n + Snm1;
+    return Sn;
+  }
+
   public static void main(String[] args) {
     int n = 5;
     // printDec(n);
     // printInc(n);
-    System.out.println(fact(n));
+    // System.out.println(fact(n));
+    System.out.println(calSum(n));
   }
 }
 
